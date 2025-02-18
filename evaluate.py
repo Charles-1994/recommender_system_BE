ad0bff21d3b472c5c99425bac4720ccce96d6893
Charles <charles.pidakala@gmail.com>
2025-02-18 01:35:17 +0100

    Initial folder setup and data_exploration.ipynb file is added

---
+++ evaluate.py
@@ -0,0 +1,23 @@
+from recommender import Recommender
+
+
+r = Recommender("./items.csv","./users.csv","./events.csv")
+
+r.analyse()
+
+r.train()
+
+
+#Evaluate recommender
+with open("./sessions.csv",'r')as f:
+    hits = 0
+    total = 0
+    for row in f.readlines():
+        parts = row.split("\t")
+        session = parts[0].split(",")
+        target_item = row[1]
+        recommended = r.recommend(session)
+        if target_item in recommended:
+            hits +=1
+        total+=1
+    print("Hits: {}/{}".format(hits, total))
---
+++ recommender.py
@@ -0,0 +1,17 @@
+class Recommender:
+    def __init__(self, items_path, users_path, events_path):
+        #YOUR CODE HERE
+        pass
+        
+    def analyse(self):
+        #YOUR CODE HERE
+        pass
+    
+    def train(self):
+        #YOUR CODE HERE
+        pass
+        
+    def recommend(self, session_items):
+        #YOUR CODE HERE
+        return []
+        
